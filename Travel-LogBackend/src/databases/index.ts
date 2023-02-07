0fd23b07f2c32a2fed08846ba5af258ee6cf8ca4
UrvashiSohaliya-blitz <124357213+UrvashiSohaliya-blitz@users.noreply.github.com>
2023-02-08 00:42:09 +0530

    Delete Travel-LogBackend directory

--- Travel-LogBackend/src/databases/index.ts
+++
@@ -1,9 +0,0 @@
-import { DB_HOST, DB_PORT, DB_DATABASE ,DB_PASSWORD} from '@config';
-
-export const dbConnection = {
-  url: `mongodb+srv://experiance:${DB_PASSWORD}@cluster0.x1jnvd2.mongodb.net/?retryWrites=true&w=majority`,
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-};
--- Travel-LogBackend/src/interfaces/question.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Question {
-    _id: String;
-    userId: String,
-    blogId: String,
-    question: String,
-    blogUser: String,
-    answer:String
-    }
